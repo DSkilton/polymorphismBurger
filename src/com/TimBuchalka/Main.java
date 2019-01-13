2bb9c9e5390c8b80a356538c6907d8333d685fe9
DSkilton <the_skiltons@hotmail.com>
2019-01-13 19:28:26 +0000

    Initial commit

--- src/com/TimBuchalka/Main.java
+++ src/com/TimBuchalka/Main.java
@@ -3,6 +3,13 @@ package com.TimBuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "white");
+    double price = hamburger.itemiseHamburger();
+    hamburger.addHamburgerAddition1("Tomato", 0.27);
+    hamburger.addHamburgerAddition2("Lettuce", 0.75);
+    hamburger.addHamburgerAddition3("Cheese", 1.13);
+    System.out.println("Total Burger price is " + hamburger.itemiseHamburger());
+
     }
 }
